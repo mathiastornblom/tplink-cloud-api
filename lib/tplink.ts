0c055ccb62a46849a9f6a7320a9b730e16b4bb0a
mathiastornblom <mathias@tornbloms.net>
2023-08-04 14:52:52 +0200

    Added P115

--- lib/tplink.ts
+++ lib/tplink.ts
@@ -20,21 +20,22 @@ FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with
 tplink-cloud-api. If not, see http://www.gnu.org/licenses/. */
 
-import axios from "axios";
-import { v4 } from "uuid";
-import { checkError } from "./api-utils";
-import device from "./device";
-import hs100 from "./hs100";
-import hs110 from "./hs110";
-import hs200 from "./hs200";
-import hs300 from "./hs300";
-import lb100 from "./lb100";
-import lb120 from "./lb120";
-import lb130 from "./lb130";
+import axios from 'axios';
+import { v4 } from 'uuid';
+import { checkError } from './api-utils';
+import device from './device';
+import hs100 from './hs100';
+import hs110 from './hs110';
+import hs200 from './hs200';
+import hs300 from './hs300';
+import lb100 from './lb100';
+import lb120 from './lb120';
+import lb130 from './lb130';
+import p115 from './p115';
 import axiosCurlirize from 'axios-curlirize';
 
-if( process.env.CURLIRIZE ) {
-  axiosCurlirize(axios);
+if (process.env.CURLIRIZE) {
+    axiosCurlirize(axios);
 }
 
 /* Example
@@ -46,214 +47,219 @@ if( process.env.CURLIRIZE ) {
 }
 */
 interface LoginResponse {
-  accountId: string;
-  regTime: string;
-  email: string;
-  token: string;
-  // there is no refresh token in response
+    accountId: string;
+    regTime: string;
+    email: string;
+    token: string;
+    // there is no refresh token in response
 }
 
 export async function login(
-  user: string,
-  passwd: string,
-  termid: string = v4()
+    user: string,
+    passwd: string,
+    termid: string = v4()
 ): Promise<TPLink> {
-  if (!user) {
-    throw new Error("missing required user parameter");
-  } else if (!passwd) {
-    throw new Error("missing required password parameter");
-  }
-
-  const request = {
-    method: "POST",
-    url: "https://wap.tplinkcloud.com",
-    params: {
-      appName: "Kasa_Android",
-      termID: termid,
-      appVer: "1.4.4.607",
-      ospf: "Android+6.0.1",
-      netType: "wifi",
-      locale: "es_ES"
-    },
-    data: {
-      method: "login",
-      url: "https://wap.tplinkcloud.com",
-      params: {
-        appType: "Kasa_Android",
-        cloudPassword: passwd,
-        cloudUserName: user,
-        terminalUUID: termid
-      }
-    },
-    headers: {
-      "User-Agent":
-        "Dalvik/2.1.0 (Linux; U; Android 6.0.1; A0001 Build/M4B30X)",
-      "Content-Type": "application/json"
+    if (!user) {
+        throw new Error('missing required user parameter');
+    } else if (!passwd) {
+        throw new Error('missing required password parameter');
     }
-  };
 
-  const response = await axios(request);
-  checkError(response);
-  const token = response.data.result.token;
-  return new TPLink(token, termid);
-}
-
-export default class TPLink {
-  token: string;
-  termid: string;
-  deviceList: any[];
-  constructor(token: string, termid: string = v4()) {
-    this.token = token;
-    this.termid = termid;
-  }
-
-  getTermId(): string {
-    return this.termid;
-  }
-  getToken(): string {
-    return this.token;
-  }
-
-  async getDeviceList(): Promise<any[]> {
     const request = {
-      method: "POST",
-      url: "https://wap.tplinkcloud.com",
-      params: {
-        appName: "Kasa_Android",
-        termID: this.termid,
-        appVer: "1.4.4.607",
-        ospf: "Android+6.0.1",
-        netType: "wifi",
-        locale: "es_ES",
-        token: this.token
-      },
-      headers: {
-        "User-Agent":
-          "Dalvik/2.1.0 (Linux; U; Android 6.0.1; A0001 Build/M4B30X)",
-          "Content-Type": "application/json"
-      },
-      data: { method: "getDeviceList" }
+        method: 'POST',
+        url: 'https://wap.tplinkcloud.com',
+        params: {
+            appName: 'Kasa_Android',
+            termID: termid,
+            appVer: '1.4.4.607',
+            ospf: 'Android+6.0.1',
+            netType: 'wifi',
+            locale: 'es_ES',
+        },
+        data: {
+            method: 'login',
+            url: 'https://wap.tplinkcloud.com',
+            params: {
+                appType: 'Kasa_Android',
+                cloudPassword: passwd,
+                cloudUserName: user,
+                terminalUUID: termid,
+            },
+        },
+        headers: {
+            'User-Agent':
+                'Dalvik/2.1.0 (Linux; U; Android 6.0.1; A0001 Build/M4B30X)',
+            'Content-Type': 'application/json',
+        },
     };
 
     const response = await axios(request);
     checkError(response);
-    return (this.deviceList = response.data.result.deviceList);
-  }
+    const token = response.data.result.token;
+    return new TPLink(token, termid);
+}
 
-  // factory to return a new device object from a name (alias) or info object, { deviceType: ..., deviceModel: ... }
-  newDevice(nameOrInfo): device {
-    if (!nameOrInfo) {
-      throw new Error("missing required parameter nameOrInfo");
-    } else if (
-      typeof nameOrInfo !== "string" &&
-      typeof nameOrInfo !== "object"
-    ) {
-      throw new Error(
-        "invalid parameter type provided for nameOrInfo; expected string or object"
-      );
+export default class TPLink {
+    token: string;
+    termid: string;
+    deviceList: any[];
+    constructor(token: string, termid: string = v4()) {
+        this.token = token;
+        this.termid = termid;
     }
 
-    let deviceInfo = nameOrInfo;
-    if (typeof nameOrInfo === "string") {
-      deviceInfo = this.findDevice(nameOrInfo);
+    getTermId(): string {
+        return this.termid;
+    }
+    getToken(): string {
+        return this.token;
     }
 
-    // https://github.com/plasticrake/tplink-smarthome-api/blob/master/src/device/index.js#L113
-    const type = deviceInfo.deviceType.toLowerCase();
-    const model = deviceInfo.deviceModel;
-    if (type.includes("bulb")) {
-      if (model && model.includes("130")) {
-        return new lb130(this, deviceInfo);
-      }
-      if (model && model.includes("120")) {
-        return new lb120(this, deviceInfo);
-      }
-      return new lb100(this, deviceInfo);
+    async getDeviceList(): Promise<any[]> {
+        const request = {
+            method: 'POST',
+            url: 'https://wap.tplinkcloud.com',
+            params: {
+                appName: 'Kasa_Android',
+                termID: this.termid,
+                appVer: '1.4.4.607',
+                ospf: 'Android+6.0.1',
+                netType: 'wifi',
+                locale: 'es_ES',
+                token: this.token,
+            },
+            headers: {
+                'User-Agent':
+                    'Dalvik/2.1.0 (Linux; U; Android 6.0.1; A0001 Build/M4B30X)',
+                'Content-Type': 'application/json',
+            },
+            data: { method: 'getDeviceList' },
+        };
+
+        const response = await axios(request);
+        checkError(response);
+        return (this.deviceList = response.data.result.deviceList);
     }
-    if (type.includes("plug")) {
-      if (model && model.includes("110")) {
-        return new hs110(this, deviceInfo);
-      }
-      return new hs100(this, deviceInfo);
+
+    // factory to return a new device object from a name (alias) or info object, { deviceType: ..., deviceModel: ... }
+    newDevice(nameOrInfo): device {
+        if (!nameOrInfo) {
+            throw new Error('missing required parameter nameOrInfo');
+        } else if (
+            typeof nameOrInfo !== 'string' &&
+            typeof nameOrInfo !== 'object'
+        ) {
+            throw new Error(
+                'invalid parameter type provided for nameOrInfo; expected string or object'
+            );
+        }
+
+        let deviceInfo = nameOrInfo;
+        if (typeof nameOrInfo === 'string') {
+            deviceInfo = this.findDevice(nameOrInfo);
+        }
+
+        // https://github.com/plasticrake/tplink-smarthome-api/blob/master/src/device/index.js#L113
+        const type = deviceInfo.deviceType.toLowerCase();
+        const model = deviceInfo.deviceModel;
+        if (type.includes('bulb')) {
+            if (model && model.includes('130')) {
+                return new lb130(this, deviceInfo);
+            }
+            if (model && model.includes('120')) {
+                return new lb120(this, deviceInfo);
+            }
+            return new lb100(this, deviceInfo);
+        }
+        if (type.includes('plug')) {
+            if (model && model.includes('110')) {
+                return new hs110(this, deviceInfo);
+            }
+            return new hs100(this, deviceInfo);
+        }
+
+        if (type.includes('switch')) {
+            if (model && model.includes('200')) {
+                return new hs200(this, deviceInfo);
+            }
+        }
+        return new device(this, deviceInfo);
     }
 
-    if (type.includes("switch")) {
-      if (model && model.includes("200")) {
-        return new hs200(this, deviceInfo);
-      }
+    findDevice(alias: string): any {
+        let deviceInfo;
+        if (alias && this.deviceList) {
+            // we first search a correspondig device by alias
+            deviceInfo = this.deviceList.find((d) => d.alias === alias);
+            // we then search a correspondig device by deviceId
+            if (!deviceInfo) {
+                deviceInfo = this.deviceList.find((d) => d.deviceId === alias);
+            }
+        }
+        if (!deviceInfo) {
+            throw new Error('invalid alias/deviceId: not found in device list');
+        }
+        return deviceInfo;
     }
-    return new device(this, deviceInfo);
-  }
 
-  findDevice(alias: string): any {
-    let deviceInfo;
-    if (alias && this.deviceList) {
-      // we first search a correspondig device by alias
-      deviceInfo = this.deviceList.find(d => d.alias === alias);
-      // we then search a correspondig device by deviceId
-      if (!deviceInfo) {
-        deviceInfo = this.deviceList.find(d => d.deviceId === alias);
-      }
+    // for an P115 smartplug
+    getP115(alias) {
+        return new p115(this, this.findDevice(alias));
     }
-    if (!deviceInfo) {
-      throw new Error("invalid alias/deviceId: not found in device list");
+
+    // for an HS100 smartplug
+    getHS100(alias) {
+        return new hs100(this, this.findDevice(alias));
     }
-    return deviceInfo;
-  }
 
-  // for an HS100 smartplug
-  getHS100(alias) {
-    return new hs100(this, this.findDevice(alias));
-  }
+    // for an HS110 smartplug
+    getHS110(alias) {
+        return new hs110(this, this.findDevice(alias));
+    }
 
-  // for an HS110 smartplug
-  getHS110(alias) {
-    return new hs110(this, this.findDevice(alias));
-  }
+    // for an HS200 smart switch
+    getHS200(alias) {
+        return new hs200(this, this.findDevice(alias));
+    }
 
-  // for an HS200 smart switch
-  getHS200(alias) {
-    return new hs200(this, this.findDevice(alias));
-  }
+    // for an HS300 smart multi-switch
+    getHS300(alias) {
+        return new hs300(this, this.findDevice(alias));
+    }
 
-  // for an HS300 smart multi-switch
-  getHS300(alias) {
-    return new hs300(this, this.findDevice(alias));
-  }
+    // for an LB100, LB110, KL110, KL50, KL60 lightbulb (dimmable)
+    getLB100(alias): lb100 {
+        return new lb100(this, this.findDevice(alias));
+    }
+    getLB110(alias) {
+        return this.getLB100(alias);
+    }
+    getKL110(alias) {
+        return this.getLB100(alias);
+    }
+    getKL50(alias) {
+        return this.getLB100(alias);
+    }
+    getKL60(alias) {
+        return this.getLB100(alias);
+    }
 
-  // for an LB100, LB110, KL110, KL50, KL60 lightbulb (dimmable)
-  getLB100(alias): lb100 {
-    return new lb100(this, this.findDevice(alias));
-  }
-  getLB110(alias) {
-    return this.getLB100(alias);
-  }
-  getKL110(alias) {
-    return this.getLB100(alias);
-  }
-  getKL50(alias) {
-    return this.getLB100(alias);
-  }
-  getKL60(alias) {
-    return this.getLB100(alias);
-  }
+    // for an LB120, KL120 lightbulb (dimmable, tunable white)
+    getLB120(alias): lb120 {
+        return new lb120(this, this.findDevice(alias));
+    }
+    getKL120(alias) {
+        return this.getLB120(alias);
+    }
 
-  // for an LB120, KL120 lightbulb (dimmable, tunable white)
-  getLB120(alias): lb120 {
-    return new lb120(this, this.findDevice(alias));
-  }
-  getKL120(alias) {
-    return this.getLB120(alias);
-  }
-
-  // for an LB130, KB130, KL130 lightbulb (dimmable, tunable white, color changing)
-  getLB130(alias) {
-    return new lb130(this, this.findDevice(alias));
-  }
-  getKB130(alias) {
-    return this.getLB130(alias);
-  }
-  getKL130(alias) {
-    return this.getLB130(alias);
-  }
+    // for an LB130, KB130, KL130 lightbulb (dimmable, tunable white, color changing)
+    getLB130(alias) {
+        return new lb130(this, this.findDevice(alias));
+    }
+    getKB130(alias) {
+        return this.getLB130(alias);
+    }
+    getKL130(alias) {
+        return this.getLB130(alias);
+    }
 }
